9c2621047808d7a2e93a08866c732d7feb22c8b1
sohailf7 <33504467+sohailf7@users.noreply.github.com>
2019-09-28 19:49:40 -0400

    Implemented flashing LEDs

--- Lab4/asm_main.s
+++ Lab4/asm_main.s
@@ -3,14 +3,20 @@ P1DIR EQU 0x40004C04
 ; Port 1 Pin Output Register
 P1OUT EQU 0x40004C02
 P1IN EQU 0x40004C00
+P1REN EQU 0x40004C06
 	
+P2REN EQU 0x40004C07 ; resistor enable
 P2DIR EQU 0x40004C05
 P2OUT EQU 0x40004C03
 P2IN EQU 0x40004C01
 
-FLAmask EQU 0x40 ; bitmask P2.6
-TSTmask EQU 0x2 ; bitmask P1.1
 YELLOWmask EQU 0x10 ; bitmask P2.4
+
+TSTmask EQU 0x2 ; bitmask P1.1
+ACKmask EQU 0x10; bitmask P1.4
+FLAmask EQU 0x40 ; bitmask P2.6
+OLAmask EQU 0x80 ; bitmask P2.7
+
 	
 
         THUMB
@@ -18,21 +24,62 @@ YELLOWmask EQU 0x10 ; bitmask P2.4
         EXPORT  asm_main
 
 asm_main
-        ; make P1.0 an output pin
-        LDR     R0, =P2DIR      ; load Dir Reg in R1
+        LDR     R0, =P2DIR      
         LDRB    R1, [R0]
-        ORR     R1, #23          ; set bit 0
-        STRB    R1, [R0]        ; store back to Dir Reg
+        ORR     R1, #255          
+        STRB    R1, [R0]        
 		
-		LDR     R0, =P1DIR      ; load Dir Reg in R1
+		; make pin 2.6 pull up configured
+;		LDR     R0, =P2REN
+;        LDRB    R1, [R0]
+;        ORR     R1, #64          
+;        STRB    R1, [R0] 
+		
+		
+		
+		LDR     R0, =P1DIR      
         LDRB    R1, [R0]
-        ORR     R1, #23          ; set bit 0
-        STRB    R1, [R0]        ; store back to Dir Reg
+        ORR     R1, #255          
+        STRB    R1, [R0]
+		
+		; TIM CODE
+		LDR     R0, =P2REN      ; load Dir Reg in R1
+		LDRB    R1, [R0]        ;
+		ORR     R1, FLAmask     ; set bit
+		STRB    R1, [R0]        ; store back to Dir Reg
+		
+		LDR     R0, =P2OUT      ; load Dir Reg in R1
+		LDRB    R1, [R0]        ;
+		ORR     R1, FLAmask		; set bit
+		STRB    R1, [R0]        ; store back to Dir Reg
+		
+		
+		
+		; RYAN BROKEN CODE
+;		; make P1.[1, 4, 6, 7] inputs pins
+;		LDR     R0, =P1DIR      
+;        LDRB    R1, [R0]
+;        MVN		R2, #0xD2
+;		AND		R1, R2
+;        STRB    R1, [R0]
+;		
+;		; enable pull configuration P1.1, P1.4
+;		LDR R0, =P1REN
+;		LDRB R1, [R0]
+;		ORR R1, #0x12
+;		STRB R1, [R0]
+;		
+;		; P1.1 pull up resistor
+;		LDR R0, =P1OUT
+;		LDRB R1, [R0]
+;		ORR R1, #0x12
+;		STRB R1, [R0]
+		
 
 loop
         BL 		allOFF
 
-        ; STATE B
+        ; start in state b
 		BL		stateB
         
 
@@ -47,10 +94,13 @@ stateA
 	BL redON
 	
 	LDR     R0, =500000
-        BL      delayMs
-	
+    BL      delayMs
+					
 	BL yellowOFF
 	BL redOFF
+	
+	LDR     R0, =500000
+    BL      delayMs
 	
 	B stateA
 
@@ -75,7 +125,8 @@ stateB
 	B stateB
 
 stateC
-	BL redON ; THIS ISNT RIGHT FOR STATE C
+	BL yellowON ; THIS ISNT RIGHT FOR STATE C
+	BL greenOFF
 	B stateC
 
 ; ------------------------------------------------------------------------------
