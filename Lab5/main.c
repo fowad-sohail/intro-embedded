9386d7a3e53150e1dad32fc9fac77877217f608c
sohailf7 <33504467+sohailf7@users.noreply.github.com>
2019-10-08 17:29:15 -0400

    Remove comments

--- Lab5/main.c
+++ Lab5/main.c
@@ -25,20 +25,12 @@ int main(void)
         j = P2IN & BIT1;
         if(j != 2)
         {
-            /**P1OUT |= 0x01; // ON LED
-
-            int i = 50000000; // Delay
-            do (i--);
-            while (i != 0);
-
-            P1OUT &= ~0x1; // OFF LED**/
-
             volatile unsigned int i;
                 for(i = 0; i<=6000; i++)
                 {
                     if((i % 2000) == 0)
                     {
-                        P1OUT ^= 0x50;      // Toggle P1.6 using exclusive-OR
+                        P1OUT ^= 0x50;      // Toggle P1.6 and P1.4 using exclusive-OR
                     }
                 }
         }
